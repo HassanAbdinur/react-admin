94265df359ebaf379392ee69e44d59a27c845703
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-23 16:05:49 -0800

    rebuilding admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import restProvider from 'ra-data-simple-rest';
-import PostList from './components/PostList';
-import PostCreate from './components/PostCreate';
-import PostEdit from './components/PostEdit';
+import jsonServerProvider from 'ra-data-json-server';
+import UserList from './components/Users';
+// import UserList from './components/UserList';
+// import UserCreate from './components/UserCreate';
+// import UserEdit from './components/UserEdit';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   return (
-    <Admin dataProvider={restProvider('http://localhost:3000')}>
-      <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
+    <Admin dataProvider={dataProvider}>
+      <Resource 
+        name='users' list={UserList} 
+        // list={UserList} 
+        // create={UserCreate} 
+        // edit={UserEdit} 
+      />
     </Admin>
   );
 }
